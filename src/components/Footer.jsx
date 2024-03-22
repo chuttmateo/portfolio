db2d2b0bb9572f149e1f433a3824886e910076ad
chuttmateo <chuttmateo0@gmail.com>
2024-03-22 14:57:30 -0300

    add card skeleton, dark mode

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     
     return (
-        <div>
+        <div className="">
             <p>{currentDate()}</p>
         </div>
     )
--- src/components/GitHubProjects.jsx
+++ src/components/GitHubProjects.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 import ProjectCard from "./ProjectCard";
 import { useEffect } from "react";
+import CardSkeleton from "./CardSkeleton";
 
 export default function GitHubPrjects() {
 
-    const [proyectos, setProyectos] = useState([]);
+    const [proyectos, setProyectos] = useState([{},{},{},{},{},{},{},{},{},{},{},{}]);
+    const [loading, setLoading] = useState(false);
     useEffect(() => {
         async function fetchProjects() {
             try {
+                setLoading(true)
                 const projects = await getProjects();
                 setProyectos(projects);
+                setLoading(false)
             } catch (error) {
                 console.error('Error fetching projects:', error);
             }
@@ -27,10 +31,17 @@ export default function GitHubPrjects() {
         <div>
             <h2 className="text-xl sm:text-3xl font-extrabold p-4">GitHub Projects</h2>
             <div className="grid justify-center gap-4 sm:grid-cols-2 md:grid-cols-3">
-                {proyectos.map((p) => {
-                    return <ProjectCard key={p.id} object={p} />
+                {loading
+                    ?
+                    proyectos.map(() => {
+                        return <CardSkeleton key={Math.random()}/>
+                    })
+                    :
+                    proyectos.map((p) => {
+                        return <ProjectCard key={p.id} object={p} />
+                    }
+                    )
                 }
-                )}
             </div>
         </div>
     )
